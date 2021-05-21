860cf8f044db23e728ad1579d7f10dc060186c0a
Liam <urehelonn@gmail.com>
2021-05-21 15:53:05 -0400

    main removed to make it as an exportable module

--- prac1_p1.go
+++ prac1_p1.go
@@ -1,16 +1,5 @@
 package main
 
-import (
-	"fmt"
-)
-
-func main() {
-	fvs := Favs()
-	for fv := range fvs {
-		fmt.Println(fvs[fv])
-	}
-}
-
 func Favs() []string {
 	return []string{"Apple", "Banana", "Orange", "Pineapple"}
 }
